6ec3367e965fa08bb83a4020289650a8b1727ead
pilanias38 <pilanias38@gmail.com>
2023-11-10 00:30:36 +0530

    litiil

--- tina/config.js
+++ tina/config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "tinacms";
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
+  branch: "main",
   clientId: "531cde0d-c800-489b-a1a0-458015755a27", // Get this from tina.io
   token: "927b455697708484322c2732614908539111c1f4", // Get this from tina.io
 
